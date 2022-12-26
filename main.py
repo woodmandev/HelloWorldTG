a8ed3b66fd87a76eff5ae5dba94c8f1781ba9fd3
woodman <shutov.dev@proton.me>
2022-12-26 14:24:31 +0200

    first test

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+TG_TOKEN="5808341596:AAHSHyUYnpgsMc5DOazWgm2Aq7CmtzT5ZI4"
 
+def start(update, context):
+    update.message.reply_text("start using bot")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+def help(update, context):
+    update.message.reply_text("you need help?")
 
+def main ():
+    updater = Updater(TG_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help))
+    updater.start_polling()
+    updater.idle()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
